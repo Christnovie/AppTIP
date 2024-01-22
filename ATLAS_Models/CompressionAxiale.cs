b896f9c9c06bcabdcd3256e8dff0008f21a4a84f
crhistnovie <56388910+crhistnovie@users.noreply.github.com>
2024-01-22 15:47:20 +0100

    compression axial begin

---
+++ ATLAS_Models/CompressionAxiale.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATLAS_Models
+{
+    public class CompressionAxiale
+    {
+        public CompressionAxiale(double Stransb,double Stransh,double Flamby, double Flambz, string  )
+        {
+
+        }
+        //Aire de la section (A)
+        public  double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        //Valeur de calcul de la contrainte axiale (σc,0,d)
+
+        public double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        public double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        public static double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        public static double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        public static double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        public static double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        public static double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        public static double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        public static double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        public static double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        public static double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        public static double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        public static double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        public static double CalAireSection()
+        {
+            new NotImplementedException();
+        }
+        //Coefficient de flambement et resultat
+        public static double FlambementCoef()
+        {
+            if
+        }
+    }
+    
+}
